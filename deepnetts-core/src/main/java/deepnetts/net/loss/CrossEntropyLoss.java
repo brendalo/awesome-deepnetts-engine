09ccb87fd975e2c8f60c4ba6b7712345165c53bc
brendalo <130975327+brendalo@users.noreply.github.com>
2021-03-08 10:33:22 +0000

    H

--- deepnetts-core/src/main/java/deepnetts/net/loss/CrossEntropyLoss.java
+++ deepnetts-core/src/main/java/deepnetts/net/loss/CrossEntropyLoss.java
@@ -23,4 +23,59 @@ package deepnetts.net.loss;
 
 import deepnetts.net.layers.SoftmaxOutputLayer;
 import deepnetts.net.NeuralNetwork;
-import java.io.Se
+import java.io.Serializable;
+
+/**
+ * Represents Cross Entropy Loss function.
+ * This function is commonly used as a loss function for multi class classification problems.
+ * 
+ * @author Zoran Sevarac <zoran.sevarac@smart4net.co>
+ */
+public class CrossEntropyLoss implements LossFunction, Serializable {
+    private final float[] outputError;
+    private float[] actualOutput;
+    private int targetIdx;
+    
+    private final SoftmaxOutputLayer outputLayer;         
+    
+
+    public CrossEntropyLoss(NeuralNetwork convNet) {
+        outputLayer = (SoftmaxOutputLayer)convNet.getOutputLayer();
+        outputError = new float[convNet.getOutputLayer().getWidth()];
+    }
+       
+    
+    /**
+     * Calculates and returns error vector for specified actual and target outputs.
+     * 
+     * @param actualOutput actual output from the neural network
+     * @param targetOutput target/desired output of the neural network
+     * @return error vector for specified actual and target outputs
+     */
+    @Override
+    public float[] calculateOutputError(float[] actualOutput,  float[] targetOutput) {
+        this.actualOutput = actualOutput;
+        
+        for (int i = 0; i < actualOutput.length; i++) { 
+            outputError[i] = actualOutput[i] - targetOutput[i]; // ovo je dL/dy izvod loss funkcije u odnosu na izlaz ovog neurona - ovo se koristi za deltu izlaznog neurona
+            if (targetOutput[i] == 1) {                        
+                targetIdx = i; // this could be set explicitly in data set in order to avoid this if
+                outputLayer.setTargetClassIdx(i);                
+            }
+        }     
+               
+        return outputError;        
+    }
+
+    
+    /**
+     * Return the total error for the current pattern 
+     * 
+     * @return 
+     */
+    @Override
+    public float getPatternError() {
+        return (float)-Math.log(actualOutput[targetIdx]);
+    }
+    
+}
